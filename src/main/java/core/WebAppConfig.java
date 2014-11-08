c9859d21f63a4fad011f96b95f7a1a4be67b1b99
Prasad <deviprasad742@gmail.com>
2014-11-08 23:08:32 +0530

    Fix for js resources

---
+++ src/main/java/core/WebAppConfig.java
@@ -0,0 +1,16 @@
+package core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+     
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
