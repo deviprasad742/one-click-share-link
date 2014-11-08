ceb85ebba23dd57b66afd23d9b793a150a10539e
Prasad <deviprasad742@gmail.com>
2014-11-08 18:14:30 +0530

    Restoring to separate controllers

--- src/main/java/core/LoginController.java
+++ src/main/java/core/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping("/")
-    public String greeting() {
+    public String loginPage() {
         return "login";
     }
 
