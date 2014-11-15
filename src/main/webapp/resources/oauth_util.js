8ea044e23fd74ad3556b258746eb728e59a7c2e5
Prasad <deviprasad742@gmail.com>
2014-11-15 19:58:01 +0530

    Testing google plus oauth api's

--- src/main/webapp/resources/oauth_util.js
+++ src/main/webapp/resources/oauth_util.js
@@ -1,9 +1,4 @@
-
-
-
-
-function loadScript(url, callback)
-{
+function loadScript(url, callback) {
     // Adding the script tag to the head as suggested before
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
@@ -21,41 +16,42 @@ function loadScript(url, callback)
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    
-    loadScript("resources/oauth.js",function() {     
-        
+
+    loadScript("resources/oauth.js", function () {
+
         var xmlhttp = new XMLHttpRequest();
         var url = "https://one-click-share-link.herokuapp.com/key";
-		xmlhttp.open("GET", url, true);
-		xmlhttp.send();
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
 
         //show success status by reverting button style.
-        xmlhttp.onreadystatechange = function() {
+        xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4) {
                 var key = xmlhttp.responseText;
                 OAuth.initialize(key);
                 console.log("OAuth initialized with key:" + key)
             }
         };
-        
-        
-    });
-    
-	var bt = document.createElement("BUTTON");
-	var text = document.createTextNode("Login");
-	bt.appendChild(text);
-	document.getElementById("send-button-div").appendChild(bt);
-	bt.style.background='white';
-	bt.style.color='black';
 
-	document.body.appendChild(document.createElement("br"));
-	bt.addEventListener("click",function(){
-        OAuth.popup("google_plus")
-            .done(function(result) {
+
+    });
+
+    var bt = document.createElement("BUTTON");
+    var text = document.createTextNode("Login");
+    bt.appendChild(text);
+    document.getElementById("send-button-div").appendChild(bt);
+    bt.style.background = 'white';
+    bt.style.color = 'black';
+
+    document.body.appendChild(document.createElement("br"));
+    bt.addEventListener("click", function () {
+        OAuth.popup("google")
+            .done(function (result) {
                 console.log("Login Result:");
                 console.log(result);
                 bt.textContent = "Logged-In";
-                result.get('/plus/v1/people/me')
+
+                result.get('oauth2/v1/userinfo')
                     .done(function (response) {
                         console.log(response);
                     })
