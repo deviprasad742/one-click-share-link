b13fa9a169cc579de5f5d75096508d0ecfd07fb5
Prasad <deviprasad742@gmail.com>
2014-11-21 02:39:53 +0530

    Testing user repository functionality

--- src/main/webapp/resources/server_login.js
+++ src/main/webapp/resources/server_login.js
@@ -1,55 +1,60 @@
 function loadScript(url, callback) {
-	// Adding the script tag to the head as suggested before
-	var head = document.getElementsByTagName('head')[0];
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = url;
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
 
-	// Then bind the event to the callback function.
-	// There are several events for cross browser compatibility.
-	script.onreadystatechange = callback;
-	script.onload = callback;
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
 
-	// Fire the loading
-	head.appendChild(script);
+    // Fire the loading
+    head.appendChild(script);
 }
 
 domain_url = "https://one-click-share-link-dev.herokuapp.com/"
 //domain_url = "https://one-click-share-link.herokuapp.com/"
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-	var token = getParam("code");
-	if (token != null) {
+    var token = getParam("code");
+    if (token != null) {
         document.getElementById("login-link").textContent = "Logged-In";
-		updateInfo(token);
-	}
+        updateInfo(token);
+    }
 
 
-	function getParam(name) {
-		if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)'))
-				.exec(location.search))
-			return decodeURIComponent(name[1]);
-	}
+    function getParam(name) {
+        if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)'))
+            .exec(location.search))
+            return decodeURIComponent(name[1]);
+    }
 
-	function updateInfo(token) {
-		var xmlhttp = new XMLHttpRequest();
+    function updateInfo(token) {
+        var xmlhttp = new XMLHttpRequest();
         var reqUrl = domain_url + "register" + "?code=" + token;
-		xmlhttp.open("POST", reqUrl, true);
-		xmlhttp.send();
+        xmlhttp.open("POST", reqUrl, true);
+        xmlhttp.send();
 
-		//show success status by reverting button style.
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == 4) {
+        //show success status by reverting button style.
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
                 var data = xmlhttp.responseText;
                 console.log(data);
-                var json = JSON.parse(data);
-				var newParagraph = document.createElement('p');
-				newParagraph.textContent = data;
-				document.body.appendChild(newParagraph);
-			}
-		};
+                var jsonData = JSON.parse(data);
 
-	}
+                var newParagraph = document.createElement('p');
+                newParagraph.textContent = jsonData.name;
+                document.body.appendChild(newParagraph);
+
+                var image = document.createElement('img');
+                image.src = jsonData.image;
+                document.body.appendChild(image);
+            }
+        };
+
+    }
 
 });
